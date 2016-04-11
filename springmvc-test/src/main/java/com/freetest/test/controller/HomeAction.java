95a417e9e00ff98709ce015d82edcba9b75dab40
freespace161 <freespace161@gmail.com>
2016-04-11 14:51:40 +0800

    homeaction

---
+++ springmvc-test/src/main/java/com/freetest/test/controller/HomeAction.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright baidu.com All right reserved. This software is the
+ * confidential and proprietary information of baidu.com ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with baidu.com.
+ */
+package com.freetest.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 类UserAction.java的实现描述：TODO 类实现描述
+ * 
+ * @author free 2015年9月25日 下午4:05:57
+ */
+@Controller
+//@RequestMapping(value = "/")
+public class HomeAction {
+
+    @RequestMapping(value = "/", method = { RequestMethod.GET })
+    public ModelAndView index() {
+        ModelAndView mav = new ModelAndView("index");
+
+        mav.addObject("user", "hello");
+        return mav;
+    }
+}
